33d657d503155e13160f0f04f1496644ec6e74b0
jucheolkang <jucheol9877@naver.com>
2025-07-31 17:46:33 +0900

    build : 프로젝트 생성

---
+++ src/main/java/com/project/questday/QuestDayApplication.java
@@ -0,0 +1,13 @@
+package com.project.questday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestDayApplication.class, args);
+    }
+
+}
